1fcc6923befe6c8d59b82bcf5c5195c99dc0d0ef
Luis <luisbarrios0110@gmail.com>
2025-05-07 22:25:12 -0500

    integrando astrojs/node y clerk en el proyecto

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,16 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss  from '@tailwindcss/vite';
+import clerk from '@clerk/astro';
+import node from '@astrojs/node'
 
 // https://astro.build/config
 export default defineConfig({
+  integrations: [clerk()],
+  adapter: node({mode: "standalone"}),
+  output:"server",
   vite: {
     plugins: [tailwindcss ()],
+
   }
 });
