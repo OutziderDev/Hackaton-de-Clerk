ff1ea4b2c2d4d0a346998e6b6598eba137a922bf
Luis <luisbarrios0110@gmail.com>
2025-05-09 17:34:53 -0500

    feat: Aside Righ V.0

---
+++ src/conts/usuarios.ts
@@ -0,0 +1,32 @@
+export const usuarios = [
+  {
+    id: 'u1',
+    nombre: 'Ana Dev',
+    username: 'anadev',
+    avatar: 'https://i.pravatar.cc/150?u=anadev',
+  },
+  {
+    id: 'u2',
+    nombre: 'Luis Backend',
+    username: 'luisbackend',
+    avatar: 'https://i.pravatar.cc/150?u=luisbackend',
+  },
+  {
+    id: 'u3',
+    nombre: 'Sara UX',
+    username: 'saraux',
+    avatar: 'https://i.pravatar.cc/150?u=saraux',
+  },
+  {
+    id: 'u4',
+    nombre: 'Carlos Front',
+    username: 'carlosfront',
+    avatar: 'https://i.pravatar.cc/150?u=carlosfront',
+  },
+  {
+    id: 'u5',
+    nombre: 'Mia Script',
+    username: 'miascript',
+    avatar: 'https://i.pravatar.cc/150?u=miascript',
+  },
+];
