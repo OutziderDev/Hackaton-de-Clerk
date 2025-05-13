fd1dc1545e742c8a81e15b84338138bf01ad0aea
Luis <luisbarrios0110@gmail.com>
2025-05-12 22:26:43 -0500

    Add Preact to use useState to development faster

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,11 @@ import tailwindcss  from '@tailwindcss/vite';
 import clerk from '@clerk/astro';
 import node from '@astrojs/node'
 
+import react from '@astrojs/react';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [clerk()],
+  integrations: [clerk(), react()],
   adapter: node({mode: "standalone"}),
   vite: {
     plugins: [tailwindcss ()],
