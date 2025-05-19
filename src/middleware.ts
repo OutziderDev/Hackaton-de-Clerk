afc713847bb7cbf372840018997d1007a023ed7a
Luis <luisbarrios0110@gmail.com>
2025-05-18 21:46:06 -0500

    Fix: Añadiendo verificación de mas rutas de inicio de sesión en el middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,7 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/astro/server";
 
  const isProtectedRoute = createRouteMatcher(["/","/formu"])
+ const isLoginRoute = createRouteMatcher(["/login"])
 
 export const onRequest = clerkMiddleware( (auth, context) => {
   const { userId } = auth();
@@ -9,7 +10,8 @@ export const onRequest = clerkMiddleware( (auth, context) => {
     return context.redirect("/login")
   }
 
-}); 
+  if (isLoginRoute(context.request) && userId) {
+    return context.redirect("/")
+  }
 
-
-/* export const onRequest = clerkMiddleware() */
+});
