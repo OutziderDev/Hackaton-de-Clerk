d3903caf72eb3bea05ab8e4b9c1a90b60999256b
Luis <luisbarrios0110@gmail.com>
2025-07-07 22:40:10 -0500

    Feat: añadir soporte para lenguajes dinámicos en la sección de publicaciones

---
+++ src/data/lenguajes.js
@@ -0,0 +1,17 @@
+export const lenguajes = [
+    { "name": "React", "color": "sky-400" },
+    { "name": "JavaScript", "color": "yellow-400" },
+    { "name": "TypeScript", "color": "blue-400" },
+    { "name": "Ruby", "color": "red-400" },
+    { "name": "PHP", "color": "purple-400" },
+    { "name": "Python", "color": "green-400" },
+    { "name": "Go", "color": "orange-400" },
+    { "name": "Java", "color": "indigo-400" },
+    { "name": "HTML", "color": "pink-400" },
+    { "name": "CSS", "color": "teal-400" },
+    { "name": "NodeJS", "color": "lime-400" },
+    { "name": "Vue", "color": "fuchsia-400" },
+    { "name": "Docker", "color": "cyan-400" },
+    { "name": "CSharp", "color": "emerald-400" },
+    { "name": "C++", "color": "gray-400" }
+  ]
